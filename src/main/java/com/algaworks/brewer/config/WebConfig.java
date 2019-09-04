0069bda0e052b5605df4c94d11cf6365c94d3ffc
Karasurage <31319694+karasurage@users.noreply.github.com>
2019-09-04 17:12:16 -0300

    Alterações de Tela
    Foram realizados as alterações na tela de cadastro de cervejas.
    Criação da página de LayoutPadrao.

--- src/main/java/com/algaworks/brewer/config/WebConfig.java
+++ src/main/java/com/algaworks/brewer/config/WebConfig.java
@@ -19,6 +19,8 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.algaworks.brewer.controller.CervejasController;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
 @Configuration
 @ComponentScan(basePackageClasses = { CervejasController.class })
 @EnableWebMvc
@@ -56,6 +58,7 @@ public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
 		SpringTemplateEngine engine = new SpringTemplateEngine();
 		engine.setEnableSpringELCompiler(true);
 		engine.setTemplateResolver(templateResolver());
+		engine.addDialect(new LayoutDialect()); // Cria um novo Layout Dialect
 		return engine;
 	}
 	
