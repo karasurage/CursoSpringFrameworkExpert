0230181fde620ae0208bfcd11971c6e82e25620b
Karasurage <31319694+karasurage@users.noreply.github.com>
2019-09-09 12:22:11 -0300

    Tentativa de Correção de Erro
    Tentantiva da correção de erro do DataSource e dando continuidade ao curso mesmo com o erro!

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,24 +2,34 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Cerveja;
+import com.algaworks.brewer.model.Origem;
+import com.algaworks.brewer.model.Sabor;
+import com.algaworks.brewer.repository.Estilos;
 
 @Controller
 public class CervejasController {
 
-//	@Autowired
-//	private Cervejas cervejas;
+	@Autowired
+	private Estilos estilos;
 	
 	@RequestMapping("/cervejas/novo")
-	public String novo(Cerveja cerveja) {
-		return "cerveja/CadastroCerveja";
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+		mv.addObject("sabores", Sabor.values()); // Inseri uma lista do ENUM para a View
+		mv.addObject("estilos", estilos.findAll()); // Inseri uma lista do Banco de Dados para a View
+		mv.addObject("origens", Origem.values()); // Inseri um array para a View
+		
+		return mv;
 	}
 	
 	/*
@@ -30,15 +40,14 @@ public class CervejasController {
 	 * RedirectAttributes --> Serve para que a mensagem não suma ao redirecionar para a página especificada (addFlashAttribute)
 	 */
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) { 
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) { 
 		if (result.hasErrors()) {
-//			cervejas.findAll(); // Apagar...
 			
 			return novo(cerveja);
 		}
 		
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso!");
-		return "redirect:/cervejas/novo";
+		return new ModelAndView("redirect:/cervejas/novo");
 	}
 	
 }
