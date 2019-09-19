9dd6f5b3be51ebafaf25839b7f49fffecae03abc
Karasurage <31319694+karasurage@users.noreply.github.com>
2019-09-19 17:50:23 -0300

    Atualização da aula

--- target/classes/static/javascripts/cliente.mascara-cpf-cnpj.js
+++ target/classes/static/javascripts/cliente.mascara-cpf-cnpj.js
@@ -4,14 +4,21 @@ Brewer.MascaraCpfCnpj = (function() {
 	
 	function MascaraCpfCnpj() {
 		this.radioTipoPessoa = $('.js-radio-tipo-pessoa');
+		this.labelCpfCnpj = $('[for=cpfOuCnpj]');
+		this.inputCpfCnpj = $('#cpfOuCnpj');
 	}
 	
 	MascaraCpfCnpj.prototype.iniciar = function() {
-		this.radioTipoPessoa.on('change', onTipoPessoaAlterando.bind(this));
+		this.radioTipoPessoa.on('change', onTipoPessoaAlterado.bind(this));
 	}
 	
-	function onTipoPessoaAlterado(event) {
-		
+	function onTipoPessoaAlterado(evento) {
+		var tipoPessoaSelecionada = $(evento.currentTarget);
+
+		this.labelCpfCnpj.text(tipoPessoaSelecionada.data('documento'));
+		this.inputCpfCnpj.mask(tipoPessoaSelecionada.data('mascara'));
+		this.inputCpfCnpj.val('');
+		this.inputCpfCnpj.removeAttr('disabled');
 	}
 	
 	return MascaraCpfCnpj;
